ef213aae0b0d0113da390cc7ccbddb10ade0bc7d
VitaliiLytvyn <58705876+VitaliiLytvyn@users.noreply.github.com>
2020-12-24 17:53:00 +0200

    Add files via upload

--- src/com/company/Singletone.java
+++ src/com/company/Singletone.java
@@ -1,11 +1,14 @@
 public final class Singleton {
-    private static Singleton _instance = null;
+    private static volatile Singleton _instance = null;
 
     private Singleton() {}
 
     public static synchronized Singleton getInstance() {
         if (_instance == null)
-            _instance = new Singleton();
+            synchronized (Singleton.class) {
+                if (_instance == null)
+                    _instance = new Singleton();
+            }
         return _instance;
     }
 }
